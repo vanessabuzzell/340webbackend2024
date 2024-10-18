0b46bee81d342a69b773136ac6addc7eb5d53e71
Vanessa Buzzell <vanessabuzzell@live.com>
2024-10-18 14:48:01 -0400

    added sql code for rebuilding when needed

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -236,4 +236,19 @@ VALUES   (
     5
   );
 
-  
+--Update GMC HUMMER
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = (10);
+
+--Inner Join for Sport car
+SELECT inv.inv_make, inv.inv_model, class.classification_name
+FROM public.inventory inv
+INNER JOIN public.classification class ON inv.classification_id = class.classification_id
+WHERE class.classification_name = 'Sport';
+
+--update with adding vehicles and its file path
+UPDATE public.inventory
+SET inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+AND inv_images = REPLACE(inv_images, '/images/', '/images/vehicles/');
+
