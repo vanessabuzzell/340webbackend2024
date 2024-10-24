b189afa2fbf9466cc1951b886c0496e0ecfb794e
Vanessa Buzzell <vanessabuzzell@live.com>
2024-10-24 18:29:40 -0400

    inventory delivery by classification

---
+++ controllers/invController.js
@@ -0,0 +1,22 @@
+const invModle = require("../models/inventory-model")
+const utilities = requrie("../utilities/")
+
+const invCont = {}
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+    const classification_id = req.params.classification_Id 
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+        title: className + "vehicles", 
+        nav,
+        grid,
+    })
+}
+
+module.exports = invCont
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-
+const inventoryRoute = require(".routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -27,7 +27,8 @@ app.set("layout", "./layouts/layout") //not at views root
 app.use(static)
 //Index route
 app.get("/", baseController.buildHome)
-
+//Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
